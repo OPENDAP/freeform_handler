0ec51afcf9e6bbb862e69dd75eccb49cec649fca
James Gallagher <jgallagher@opendap.org>
1998-11-10 03:46:19 +0000

    Various fixes for the fields of std_args.

--- ff_read.c
+++ ff_read.c
@@ -7,6 +7,9 @@
 
 /*
  * $Log: ff_read.c,v $
+ * Revision 1.8  1998/11/10 03:46:19  jimg
+ * Various fixes for the fields of std_args.
+ *
  * Revision 1.7  1998/10/29 16:58:47  mao
  * turned off error prompting
  *
@@ -30,7 +33,7 @@
 
 #include "config_ff.h"
 
-static char rcsid[] __unused__ ={"$Id: ff_read.c,v 1.7 1998/10/29 16:58:47 mao Exp $"};
+static char rcsid[] __unused__ ={"$Id: ff_read.c,v 1.8 1998/11/10 03:46:19 jimg Exp $"};
 
 #include <freeform.h>
 
@@ -41,93 +44,95 @@ long
 read_ff(char *dataset, char *if_file, char *o_format, char *o_buffer, 
 	unsigned long bsize)
 {
-  int error = 0;
-  FF_BUFSIZE_PTR newform_log = NULL;
-  FF_BUFSIZE_PTR bufsz = NULL;
-  FF_STD_ARGS_PTR std_args = NULL;
+    int error = 0;
+    FF_BUFSIZE_PTR newform_log = NULL;
+    FF_BUFSIZE_PTR bufsz = NULL;
+    FF_STD_ARGS_PTR std_args = NULL;
   
-  std_args = ff_create_std_args();
-  if (!std_args)
-    {
-      error = ERR_MEM_LACK;
-      goto main_exit;
+    std_args = ff_create_std_args();
+    if (!std_args) {
+	error = ERR_MEM_LACK;
+	goto main_exit;
     }
 
-  /* set the std_arg structure values **/
-  std_args->error_prompt = FALSE;
-  std_args->user.is_stdin_redirected = 0;
-  std_args->input_file = dataset;
-  std_args->input_format_file = if_file;
-  std_args->output_file =	NULL;
-  std_args->output_format_buffer = o_format;
-  std_args->log_file = "/dev/null"; 
-  /* Define DBG (as per dap/debug.h) to get a log file from FreeForm. 9/8/98
-     jhrg */
-  DBG(std_args->log_file = "/tmp/ffdods.log"); 
+    /* set the std_arg structure values **/
+    std_args->error_prompt = FALSE;
+    std_args->user.is_stdin_redirected = 0;
+    std_args->input_file = dataset;
+    std_args->input_format_file = if_file;
+    std_args->output_file =	NULL;
+    std_args->output_format_buffer = o_format;
+    std_args->log_file = "/dev/null"; 
+    /* Define DBG (as per dap/debug.h) to get a log file from FreeForm. 9/8/98
+       jhrg */
+    DBG(std_args->log_file = "/tmp/ffdods.log"); 
 
-  bufsz = (FF_BUFSIZE_PTR)memMalloc(sizeof(FF_BUFSIZE), "bufsz");
-  bufsz->usage = 1;
-  bufsz->buffer = o_buffer;
-  bufsz->total_bytes = (FF_BSS_t)bsize;
-  bufsz->bytes_used = (FF_BSS_t)0;
-  std_args->output_bufsize = bufsz; 
+    bufsz = (FF_BUFSIZE_PTR)memMalloc(sizeof(FF_BUFSIZE), "bufsz");
+    bufsz->usage = 1;
+    bufsz->buffer = o_buffer;
+    bufsz->total_bytes = (FF_BSS_t)bsize;
+    bufsz->bytes_used = (FF_BSS_t)0;
+    std_args->output_bufsize = bufsz; 
   
-  newform_log = ff_create_bufsize(SCRATCH_QUANTA);
-  if (!newform_log)
-    {
-      error = ERR_MEM_LACK;
-      goto main_exit;
+    newform_log = ff_create_bufsize(SCRATCH_QUANTA);
+    if (!newform_log) {
+	error = ERR_MEM_LACK;
+	goto main_exit;
     }
   
-  error = newform(std_args, newform_log, stderr);
+    error = newform(std_args, newform_log, stderr);
 
 #ifdef TEST  
-  char log_file_write_mode[4];
-  /* Is user asking for both error logging and a log file? */
-  if (std_args->error_log && newform_log)
-    {
-      if (strcmp(std_args->error_log, std_args->log_file))
+    char log_file_write_mode[4];
+    /* Is user asking for both error logging and a log file? */
+    if (std_args->error_log && newform_log)  {
+	if (strcmp(std_args->error_log, std_args->log_file))
+	    strcpy(log_file_write_mode, "w");
+	else
+	    strcpy(log_file_write_mode, "a");
+      
+    }
+    else if (newform_log)
 	strcpy(log_file_write_mode, "w");
-      else
-	strcpy(log_file_write_mode, "a");
-      
-    }
-  else if (newform_log)
-    strcpy(log_file_write_mode, "w");
     
-  FILE *fp = NULL;
+    FILE *fp = NULL;
       
-  fp = fopen(std_args->log_file, log_file_write_mode);
-  if (fp)
-    {
-      size_t bytes_written = fwrite(newform_log->buffer, 1, (size_t)newform_log->bytes_used, fp);
+    fp = fopen(std_args->log_file, log_file_write_mode);
+    if (fp) {
+	size_t bytes_written = fwrite(newform_log->buffer, 1, (size_t)newform_log->bytes_used, fp);
       
-      if (bytes_written != (size_t)newform_log->bytes_used)
-	error = err_push(ERR_WRITE_FILE, "Wrote %d bytes of %d to %s", (int)bytes_written, (int)newform_log->bytes_used, std_args->log_file);
+	if (bytes_written != (size_t)newform_log->bytes_used)
+	    error = err_push(ERR_WRITE_FILE, "Wrote %d bytes of %d to %s", (int)bytes_written, (int)newform_log->bytes_used, std_args->log_file);
       
-      fclose(fp);
+	fclose(fp);
     }
-  else
-    error = err_push(ERR_CREATE_FILE, std_args->log_file);
+    else
+	error = err_push(ERR_CREATE_FILE, std_args->log_file);
   
-  if (std_args->user.is_stdin_redirected)
-    ff_destroy_bufsize(std_args->input_bufsize);
+    if (std_args->user.is_stdin_redirected)
+	ff_destroy_bufsize(std_args->input_bufsize);
 #endif //TEST  
 
-  ff_destroy_bufsize(newform_log);
+#if 0
+    /* As per the above, this should not be called if std_args->log_file is
+       null. 9/25/98 jhrg. */
+    ff_destroy_bufsize(newform_log);
+#endif
 
  main_exit:
 
-  err_disp(std_args);
+    err_disp(std_args);
 
-  if (std_args)
-    ff_destroy_std_args(std_args);
+    if (std_args)
+	ff_destroy_std_args(std_args);
   
-  /* return size; */
-  return bufsz->bytes_used;
-  
+    /* return size; */
+#if 1
+    return 1;
+#else
+    return bufsz->bytes_used;
+#endif
 }
-
 
 #ifdef TEST 
 
