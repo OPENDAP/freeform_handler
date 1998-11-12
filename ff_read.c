5d2485b41ece52387e9040e3c1ca1219d1eb9bfa
James Gallagher <jgallagher@opendap.org>
1998-11-12 05:42:40 +0000

    Changed defines so that you can build the test code separately from the error logging code. I don't really understand the latter.

--- ff_read.c
+++ ff_read.c
@@ -7,6 +7,10 @@
 
 /*
  * $Log: ff_read.c,v $
+ * Revision 1.9  1998/11/12 05:42:40  jimg
+ * Changed defines so that you can build the test code separately from the
+ * error logging code. I don't really understand the latter.
+ *
  * Revision 1.8  1998/11/10 03:46:19  jimg
  * Various fixes for the fields of std_args.
  *
@@ -33,7 +37,7 @@
 
 #include "config_ff.h"
 
-static char rcsid[] __unused__ ={"$Id: ff_read.c,v 1.8 1998/11/10 03:46:19 jimg Exp $"};
+static char rcsid[] __unused__ ={"$Id: ff_read.c,v 1.9 1998/11/12 05:42:40 jimg Exp $"};
 
 #include <freeform.h>
 
@@ -60,7 +64,7 @@ read_ff(char *dataset, char *if_file, char *o_format, char *o_buffer,
     std_args->user.is_stdin_redirected = 0;
     std_args->input_file = dataset;
     std_args->input_format_file = if_file;
-    std_args->output_file =	NULL;
+    std_args->output_file = NULL;
     std_args->output_format_buffer = o_format;
     std_args->log_file = "/dev/null"; 
     /* Define DBG (as per dap/debug.h) to get a log file from FreeForm. 9/8/98
@@ -82,7 +86,7 @@ read_ff(char *dataset, char *if_file, char *o_format, char *o_buffer,
   
     error = newform(std_args, newform_log, stderr);
 
-#ifdef TEST  
+#ifdef TEST_LOGGING
     char log_file_write_mode[4];
     /* Is user asking for both error logging and a log file? */
     if (std_args->error_log && newform_log)  {
@@ -99,10 +103,13 @@ read_ff(char *dataset, char *if_file, char *o_format, char *o_buffer,
       
     fp = fopen(std_args->log_file, log_file_write_mode);
     if (fp) {
-	size_t bytes_written = fwrite(newform_log->buffer, 1, (size_t)newform_log->bytes_used, fp);
+	size_t bytes_written = fwrite(newform_log->buffer, 1, 
+				      (size_t)newform_log->bytes_used, fp);
       
 	if (bytes_written != (size_t)newform_log->bytes_used)
-	    error = err_push(ERR_WRITE_FILE, "Wrote %d bytes of %d to %s", (int)bytes_written, (int)newform_log->bytes_used, std_args->log_file);
+	    error = err_push(ERR_WRITE_FILE, "Wrote %d bytes of %d to %s", 
+			     (int)bytes_written, 
+			     (int)newform_log->bytes_used, std_args->log_file);
       
 	fclose(fp);
     }
@@ -113,11 +120,7 @@ read_ff(char *dataset, char *if_file, char *o_format, char *o_buffer,
 	ff_destroy_bufsize(std_args->input_bufsize);
 #endif //TEST  
 
-#if 0
-    /* As per the above, this should not be called if std_args->log_file is
-       null. 9/25/98 jhrg. */
     ff_destroy_bufsize(newform_log);
-#endif
 
  main_exit:
 
@@ -126,15 +129,16 @@ read_ff(char *dataset, char *if_file, char *o_format, char *o_buffer,
     if (std_args)
 	ff_destroy_std_args(std_args);
   
-    /* return size; */
-#if 1
-    return 1;
-#else
     return bufsz->bytes_used;
-#endif
 }
 
 #ifdef TEST 
+
+#define OUTPUT_FMT_STR "binary_output_data \"output\"\n\
+year 1 4 int32 0\n\
+day 5 8 int32 0\n\
+time 9 9 text 0\n\
+DODS_URL 10 135 text 0"
 
 int main(int argc, char *argv[])
 {
@@ -153,19 +157,18 @@ int main(int argc, char *argv[])
     in_format = malloc(strlen(argv[2]) + 1); 
     strcpy(in_format, argv[2]);
 
-    /* output format writen in the buffer */
-    out_format = malloc(250);
-    strcpy(out_format, "ascii_output_data \"data st\" \nfvar1 1 4 ARRAY[\"records\"1 to 101] of int32 0");
+    /* output format written in the buffer */
+    out_format = malloc(strlen(OUTPUT_FMT_STR) + 1);
+    strcpy(out_format, OUTPUT_FMT_STR);
 
-    /* size of data expecting to read (can be oversized)*/
+    /* size of data expecting to read (can be oversized) */
     data_size = atoi(argv[3]);
     data = malloc(data_size);
-
 
     printf("Converting to the following format:\n%s\n",out_format);
     bytes_in = read_ff(datafile, in_format, out_format, data, data_size);
 
-    printf("Bytes read: %d\n", bytes_in);
+    printf("Bytes read: %ld\n", bytes_in);
     if (bytes_in > 0)
         fwrite(data, 1, bytes_in, stdout);
     exit(0);
