7326e0948c77bb8aea8fb90a504b990d8f2f61cd
James Gallagher <jgallagher@opendap.org>
1998-12-28 19:07:33 +0000

    Initial version of the DODS_Time object

---
+++ DODS_Time.cc
@@ -0,0 +1,269 @@
+
+// (c) COPYRIGHT URI/MIT 1998
+// Please read the full copyright statement in the file COPYRIGH.  
+//
+// Authors:
+//      jhrg,jimg       James Gallagher (jgallagher@gso.uri.edu)
+
+//
+// Implementation of the DODS Time class
+
+// $Log: DODS_Time.cc,v $
+// Revision 1.1  1998/12/28 19:07:33  jimg
+// Initial version of the DODS_Time object
+//
+
+#include "config_dap.h"
+
+static char rcsid[] __unused__ ="$Id: DODS_Time.cc,v 1.1 1998/12/28 19:07:33 jimg Exp $";
+
+#ifdef __GNUG__
+#pragma implementation
+#endif
+
+#include <stdio.h>
+#include <assert.h>
+
+#include <string>
+#include <String.h>
+#include <strstream.h>
+#include <iomanip.h>
+
+#include "BaseType.h"
+#include "DODS_Time.h"
+#include "debug.h" 
+#include "Error.h"
+
+double DODS_Time::_eps = 1.0e-6;
+
+static String time_syntax_string = \
+"Invalid time: times must be given as hh:mm or hh:mm:ss with an optional\n\
+suffix of GMT or UTC. In addition, 0 <= hh <=23, 0 <= mm <= 59 and\n\
+0 <= ss <= 59.999999";
+
+static inline double
+compute_ssm(int hh, int mm, double ss)
+{
+    return ((hh * 60 + mm) * 60) + ss;
+}
+
+static string
+extract_argument(BaseType *arg)
+{
+    if (arg->type() != dods_str_c)
+	throw Error(malformed_expr, "A DODS string argument is required.");
+    
+    // Use String until conversion of String to string is complete. 9/3/98
+    // jhrg
+    String *sp = 0;
+    arg->buf2val((void **)&sp);
+    string s = sp->chars();
+    delete sp;
+
+    DBG(cerr << "s: " << s << endl);
+
+    return s;
+}
+
+bool
+DODS_Time::OK()
+{
+    return _hours >= 0 && _hours <= 23
+	&& _minutes >= 0 && _minutes <= 59
+	&& _seconds >= 0.0 && _seconds < 60.0;
+}
+
+// Public mfincs.
+
+// Don't test this ctor with OK since a null Time is not OK for use but we
+// want to be able to make one and then call set_time(). 12/16/98 jhrg
+
+DODS_Time::DODS_Time(): _hours(-1), _minutes(-1), _seconds(-1),
+    _sec_since_midnight(-1), _gmt(false)
+{
+}
+
+DODS_Time::DODS_Time(string time_str)
+{
+    set_time(time_str);
+}
+
+DODS_Time::DODS_Time(BaseType *arg)
+{
+    set_time(extract_argument(arg));
+}
+
+DODS_Time::DODS_Time(int hh, int mm, bool gmt = false):
+    _hours(hh), _minutes(mm), _seconds(0), _gmt(gmt)
+{
+    _sec_since_midnight = compute_ssm(hh, mm, 0);
+    if (!OK())
+	throw Error(malformed_expr, time_syntax_string);
+}
+
+DODS_Time::DODS_Time(int hh, int mm, double ss, bool gmt = false):
+    _hours(hh), _minutes(mm), _seconds(ss), _gmt(gmt)
+{
+    _sec_since_midnight = compute_ssm(hh, mm, ss);
+    if (!OK())
+	throw Error(malformed_expr, time_syntax_string);
+}
+
+void
+DODS_Time::set_time(string time)
+{
+        // Parse the date_str.
+    istrstream iss(time.data());
+    char c;
+    iss >> _hours;
+    iss >> c;
+    iss >> _minutes;
+
+    // If there are two colons, assume hours:minutes:seconds.
+    if (time.find_first_of(":") != time.find_last_of(":")) {
+	iss >> c;
+	iss >> _seconds;
+    }
+    
+    _sec_since_midnight = compute_ssm(_hours, _minutes, _seconds);
+
+    string gmt;
+    iss >> gmt;
+    if (gmt == "GMT" || gmt == "gmt" || gmt == "UTC" || gmt == "utc")
+	_gmt = true;
+    else
+	_gmt = false;
+
+    if (!OK())
+	throw Error(malformed_expr, time_syntax_string);
+}    
+
+void
+DODS_Time::set_time(BaseType *arg)
+{
+    set_time(extract_argument(arg));
+}    
+
+void 
+DODS_Time::set_time(int hh, int mm, bool gmt = false)
+{
+    set_time(hh, mm, 0, gmt);
+}
+
+void 
+DODS_Time::set_time(int hh, int mm, double ss, bool gmt = false)
+{
+   _hours = hh;
+   _minutes = mm;
+   _seconds = ss;
+   _gmt = gmt;
+   _sec_since_midnight = compute_ssm(hh, mm, ss);
+
+    if (!OK())
+	throw Error(malformed_expr, time_syntax_string);
+}
+
+double
+DODS_Time::get_epsilon() const
+{
+    return _eps;
+}
+
+void
+DODS_Time::set_epsilon(double eps)
+{
+    _eps = eps;
+}
+
+int
+operator==(DODS_Time &t1, DODS_Time &t2)
+{
+    return t1.seconds_since_midnight() + t1._eps >= t2.seconds_since_midnight()
+    && t1.seconds_since_midnight() - t2._eps <= t2.seconds_since_midnight();
+}
+
+int
+operator!=(DODS_Time &t1, DODS_Time &t2)
+{
+    return !(t1 == t2);
+}
+
+// The relational ops > and < are possibly flaky. Note that the Intel machines
+// and the Sparc machines *do* represent floating point numbers slightly
+// differently. 
+
+int
+operator>(DODS_Time &t1, DODS_Time &t2)
+{
+    return t1.seconds_since_midnight() > t2.seconds_since_midnight();
+}
+
+int
+operator>=(DODS_Time &t1, DODS_Time &t2)
+{
+    return t1 > t2 || t1 == t2;
+}
+
+int
+operator<(DODS_Time &t1, DODS_Time &t2)
+{
+    return t1.seconds_since_midnight() < t2.seconds_since_midnight();
+}
+
+int
+operator<=(DODS_Time &t1, DODS_Time &t2)
+{
+    return t1 < t2 || t1 == t2;
+}
+
+double
+DODS_Time::seconds_since_midnight() const
+{
+    return _sec_since_midnight;
+}
+
+int
+DODS_Time::hours() const
+{
+    return _hours;
+}
+
+int
+DODS_Time::minutes() const
+{
+    return _minutes;
+}
+
+double
+DODS_Time::seconds() const
+{
+    return _seconds;
+}
+
+bool
+DODS_Time::gmt() const
+{
+    return _gmt;
+}
+
+string
+DODS_Time::string_rep(bool gmt) const
+{
+    ostrstream oss;
+    // Pad with leading zeros and use fixed fields of two chars for hours and
+    // minutes. Make sure that seconds < 10 have a leading zero but don't
+    // require their filed to have `precision' digits if they are all zero.
+    oss << setfill('0') << setw(2) << _hours << ":" 
+	<< setfill('0') << setw(2) << _minutes << ":" 
+	<< setfill('0') << setw(2) << setprecision(6) << _seconds << " ";
+
+    if (_gmt)
+	oss << "GMT";
+    
+    oss << ends;
+
+    string time_str = oss.str();
+    oss.freeze(0);
+
+    return time_str;
+}
---
+++ DODS_Time.h
@@ -0,0 +1,183 @@
+
+// -*- C++ -*-
+
+// (c) COPYRIGHT URI/MIT 1998
+// Please read the full copyright statement in the file COPYRIGH.  
+//
+// Authors:
+//      jhrg,jimg       James Gallagher (jgallagher@gso.uri.edu)
+
+// $Log: DODS_Time.h,v $
+// Revision 1.1  1998/12/28 19:07:33  jimg
+// Initial version of the DODS_Time object
+//
+
+#ifndef _dods_time_h
+#define _dods_time_h
+
+#ifdef __GNUG__
+#pragma interface
+#endif
+
+#include <string>
+
+#include "BaseType.h"
+
+/** The DODS Time object. This provides a way to translate between various
+    representations of time. This class is designed to be compatible with the
+    DODS\_Date class so that dates and times may be usd together in relational
+    expressions. 
+
+    NB: The seconds part of a DODS_Time may contain fractional components. To
+    test for equality of times, this class tests to see if the two times fall
+    within a neighborhood whose size is determined by an epsilon value which
+    is 1.0e-6 by default. Thus seconds may have precision down to the
+    micro-second (depending on the underlying hardware). See the
+    set\_epsilon() and get\_epsilon() mfuncs. The epsilon value is a static
+    class member; same value is used for all instances of the class, and when
+    changed, is changed at that time for all instances.
+
+    @see DODS\_Date
+    @author James Gallagher */
+
+class DODS_Time {
+private:
+    int _hours;			// 0--23; regular wall time
+    int _minutes;		// 0--59
+    double _seconds;		// 0--59.9...
+    double _sec_since_midnight;
+    bool _gmt;
+
+    /** Class invariant */
+    bool OK();
+
+    static double _eps;		// defined as 1.0e-6 in DODS_Time.cc
+
+public:
+    /** @name Constructors */
+
+    //@{
+    /** The string may be either of the form `hh:mm:ss' or `hh:mm'. In the
+	later case the seconds are assumed to be zero. In addition the string
+	may have the suffix `GMT' or `UTC' indicating that the time is in
+	Greenwich Mean Time.
+
+	@param time The time string. */
+
+    DODS_Time(string time);
+
+    /** Use the DODS String to set the time.
+
+	@see DODS_Time(string).
+	@param arg A DODS String variable, passed as a BaseType pointer. */
+
+    DODS_Time(BaseType *arg);
+
+    /**
+       @param hh The hours, 0-23.
+       @param mm The minutes, 0-59.
+       @param gmt True if the time is a GMT time, false otherwise. */
+
+    DODS_Time(int hh, int mm, bool gmt = false);
+
+    /**
+       @param hh The hours, 0--23.
+       @param mm The minutes, 0--59.
+       @param ss The seconds, 0--59. May contain a fractional component.
+       @param gmt True if the time is a GMT time, false otherwise. */
+
+    DODS_Time(int hh, int mm, double ss, bool gmt = false);
+
+    /** Null ctor. Use this when builidng an empty instance of DODS_Time. 
+
+	NB: This won't pass the class invariant. */
+
+    DODS_Time();
+    //@}
+
+    /** @name Assigment */
+
+    //@{
+    /** @param time The time string.
+	@see DODS_Time(string). */
+
+    void set_time(string time);
+
+    /** @param arg The time string wrapped in a DODS String.
+	@see DODS_Time(Basetype *). */
+
+    void set_time(BaseType *arg);
+
+    /** 
+       @param hh The hours, 0-23.
+       @param mm The minutes, 0-59.
+       @param gmt True if the time is a GMT time, false otherwise.
+       @see DODS_Time(int, int, bool). */
+
+    void set_time(int hh, int mm, bool gmt = false);
+
+    /**
+       @param hh The hours, 0--23.
+       @param mm The minutes, 0--59.
+       @param ss The seconds, 0--59. May contain a fractional component.
+       @param gmt True if the time is a GMT time, false otherwise. 
+       @see DODS_Time(int, int, double, bool). */
+
+    void set_time(int hh, int mm, double ss, bool gmt = false);
+    //@}
+
+    /** Get the value of epsilon used for equality tests. */
+
+    double get_epsilon() const;
+
+    /** Set the value of epsilon used for equality tests. By default the
+	value is 0.000001 (10e-6). */
+
+    void set_epsilon(double eps);
+
+    /** @name Relational operators. */
+
+    //@{
+    ///
+    friend int operator==(DODS_Time &t1, DODS_Time &t2);
+    ///
+    friend int operator!=(DODS_Time &t1, DODS_Time &t2);
+    ///
+    friend int operator<(DODS_Time &t1, DODS_Time &t2);
+    ///
+    friend int operator>(DODS_Time &t1, DODS_Time &t2);
+    ///
+    friend int operator<=(DODS_Time &t1, DODS_Time &t2);
+    ///
+    friend int operator>=(DODS_Time &t1, DODS_Time &t2);
+    //@}
+
+    /** Get the number of seconds since midnight.
+
+	@return The number of seconds since midnight. */
+
+    double seconds_since_midnight() const;
+    
+    /** @name Accessor functions.*/
+
+    //@{
+    /** @return The number of hours. */
+    int hours() const;
+
+    /** @return The number of minutes. */
+    int minutes() const;
+
+    /** @return THe number of seconds. */
+    double seconds() const;
+
+    /** @return True if the time is a GMT time, false otherwise. */
+    bool gmt() const;
+
+    /** @param gmt If true append the suffix `GMT' to the time if it a GMT
+	time. If false, ignore gmt. True by default.
+	@return The string representation for this date. */
+    string string_rep(bool gmt = true) const;
+    //@}
+};
+
+#endif // _dods_time_h
