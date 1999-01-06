61773c385047e34f63ae0d74627ee201eb42f5b6
James Gallagher <jgallagher@opendap.org>
1999-01-06 22:59:00 +0000

    Added

---
+++ ff-test/test-files.tcl
@@ -0,0 +1,57 @@
+
+#
+# $Id: test-files.tcl,v 1.1 1999/01/06 22:58:57 jimg Exp $
+#
+# Files to use for testing the FreeForm server programs.
+
+set avhrr "ff-test/1998-6-avhrr.dat"
+set avhrr_das "ff-test/avhrr_das.txt"
+set avhrr_dds "ff-test/avhrr_dds.txt"
+
+set jplpath "ff-test/jplpath.dat"
+set jplpath_das "ff-test/jplpath_das.txt"
+set jplpath_dds "ff-test/jplpath_dds.txt"
+
+set test0 "ff-test/test0.dat"
+set test0_das "ff-test/test0_das.txt"
+set test0_dds "ff-test/test0_dds.txt"
+
+set test1 "ff-test/test1.dat"
+set test1_das "ff-test/test1_das.txt"
+set test1_dds "ff-test/test1_dds.txt"
+
+set test2 "ff-test/test2.dat"
+set test2_das "ff-test/test2_das.txt"
+set test2_dds "ff-test/test2_dds.txt"
+
+set test3 "ff-test/test3.dat"
+set test3_das "ff-test/test3_das.txt"
+set test3_dds "ff-test/test3_dds.txt"
+
+set test4 "ff-test/test4.dat"
+set test4_das "ff-test/test4_das.txt"
+set test4_dds "ff-test/test4_dds.txt"
+
+set test5 "ff-test/test5.dab"
+set test5_das "ff-test/test5_das.txt"
+set test5_dds "ff-test/test5_dds.txt"
+
+set test6 "ff-test/test6.dab"
+set test6_das "ff-test/test6_das.txt"
+set test6_dds "ff-test/test6_dds.txt"
+
+set test7 "ff-test/test7.dat"
+set test7_das "ff-test/test7_das.txt"
+set test7_dds "ff-test/test7_dds.txt"
+
+set test8 "ff-test/test8.dat"
+set test8_das "ff-test/test8_das.txt"
+set test8_dds "ff-test/test8_dds.txt"
+
+set test9 "ff-test/test9.dat"
+set test9_das "ff-test/test9_das.txt"
+set test9_dds "ff-test/test9_dds.txt"
+
+set test10 "ff-test/test10.dat"
+set test10_das "ff-test/test10_das.txt"
+set test10_dds "ff-test/test10_dds.txt"
