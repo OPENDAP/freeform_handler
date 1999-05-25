37b79574bb524441dab0767e722d7a161da67e77
Dan Holloway <dholloway@opendap.org>
1999-05-25 13:56:49 +0000

    Added new DODS_Decimal_Year server-side function.

---
+++ DODS_Decimal_Year_Factory.cc
@@ -0,0 +1,43 @@
+
+// (c) COPYRIGHT URI/MIT 1998
+// Please read the full copyright statement in the file COPYRIGHT.
+//
+// Authors:
+//      jhrg,jimg       James Gallagher (jgallagher@gso.uri.edu)
+
+// Implementation of the DODS_Decimal_Year_Factory class
+
+
+#include "config_dap.h"
+
+static char rcsid[] not_used ="$Id: DODS_Decimal_Year_Factory.cc,v 1.1 1999/05/25 13:56:49 dan Exp $";
+
+#ifdef __GNUG__
+#pragma implementation
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Error.h"
+#include "DODS_Decimal_Year_Factory.h"
+
+// Build DODS_Date_Factory and DODS_Time_Factory objects using the DAS
+// information. 
+
+DODS_Decimal_Year_Factory::DODS_Decimal_Year_Factory(DDS &dds, DAS &das) :
+  _ddf(dds, das), _dtf(dds, das)
+{
+}
+
+DODS_Decimal_Year
+DODS_Decimal_Year_Factory::get()
+{
+    DODS_Date d = _ddf.get();
+    DODS_Time t = _dtf.get();
+
+    return DODS_Decimal_Year(d, t);
+}
+
+
+
